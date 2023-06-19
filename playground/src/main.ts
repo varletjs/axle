2fc69232e63879bcbbad4fc3e765ae589bbf5209
haoziqaq <357229046@qq.com>
2023-06-20 01:45:34 +0800

    types: fix types

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,8 @@
 import App from './App.vue'
 import { createApp } from 'vue'
+import { createUseAxle } from '@varlet/axle/use'
 import '@varlet/touch-emulator'
+
+console.log(createUseAxle)
 
 createApp(App).mount('#app')

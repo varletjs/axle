255236f1796eeb31fb6c15864396b640719a2feb
xiaomo <wegi866@gmail.com>
2023-12-01 18:22:37 +0800

    feat: add built-in timeout interceptor (#4)

---
+++ src/interceptors/responseTimeoutInterceptor.ts
@@ -0,0 +1,23 @@
+import type { AxiosInterceptorOptions, AxiosResponse } from 'axios'
+import type { ResponseInterceptor } from '../instance'
+
+export interface ResponseTimeoutInterceptorOptions {
+  normalizeErrorCode?: string
+  axiosInterceptorOptions?: AxiosInterceptorOptions
+}
+
+export function responseTimeoutInterceptor(options: ResponseTimeoutInterceptorOptions = {}): ResponseInterceptor {
+  return {
+    onFulfilled(response) {
+      return response
+    },
+    onRejected(error) {
+      if ((error.code == 'ECONNABORTED' && error.message.indexOf('timeout') != -1) || error.code === 'ETIMEDOUT') {
+        error.code = options?.normalizeErrorCode || 'TIMEOUT'
+      }
+
+      return Promise.reject(error)
+    },
+    options: options.axiosInterceptorOptions,
+  }
+}
